70a2b12cc8b612db7e5973f7c410d8110976b8b2
Michael Roumanos <michael.roumanos@thehumangeo.com>
2018-12-13 08:54:33 -0500

    modified scripts to run >1 Hadoop svc per container

--- daemon/start.sh
+++ daemon/start.sh
@@ -1,48 +1,79 @@
 #!/bin/bash
 
-SERVICE=$1
-MODULE=$2
-
-usage="Usage: start.sh [service] [module] (e.g. start.sh hadoop namenode)"
-
+# check for proper number of arguments
 if [ $# -le 1 ]; then
-  echo $usage
+  echo "Usage: start.sh [service1] [module1] [service2] [module2] .. (e.g. start.sh hadoop namenode)"
   exit 1
 fi
 
-echo "========== SERVICE =========="
-echo "$SERVICE-daemon.sh start $MODULE"
-
-echo "========== ENVIRONMENT =========="
-source ~/.bashrc
-echo $(printenv)
-
-echo "========== STARTING =========="
-outfile=$($SERVICE-daemon.sh start $MODULE | awk '{print $NF}' )
-base=$(basename -- ${outfile::-4})
-logfile=$base.log
-echo "$SERVICE $MODULE service started"
-echo "logs written to $logfile"
-
-echo "========== RUNNING =========="
-pidfile=/tmp/${base::-$(echo $MODULE | wc -m)}.pid
-if [ ! -f $pidfile ]; then
-    echo "daemon NOT running"
-    echo "see logs in $logfile"
+# check for multiples of two
+if [ ! $(($# % 2)) -eq 0 ]; then
+    echo "provide pairs of two arguments, service and module"
     exit 1
-else
-    pid=$(cat $pidfile)
-    echo "daemon running at $pid"
 fi
 
-trap exit_daemon SIGINT SIGTERM
+declare -a SERVICES=()
+declare -a MODULES=()
+declare -a PIDS=()
+
+for((iter=1;iter<$#;iter+=2)) {
+    SERVICES[${#SERVICES[@]}]=${!iter}
+    jiter=$((iter+1))
+    MODULES[${#MODULES[@]}]=${!jiter}
+}
+
+for((iter=0;iter<${#SERVICES[@]};iter+=1)) {
+
+    SERVICE=${SERVICES[$iter]}
+    MODULE=${MODULES[$iter]}
+
+    echo "========== SERVICE =========="
+    echo "$SERVICE-daemon.sh start $MODULE"
+
+    echo "========== ENVIRONMENT =========="
+    source ~/.bashrc
+    echo $(printenv)
+
+    echo "========== STARTING =========="
+    outfile=$($SERVICE-daemon.sh start $MODULE | awk '{print $NF}' )
+    base=$(basename -- ${outfile::-4})
+    logfile=$base.log
+    echo "$SERVICE $MODULE service started"
+    echo "logs written to $logfile"
+
+    echo "========== RUNNING =========="
+    pidfile=/tmp/${base::-$(echo $HOSTNAME | wc -m)}.pid
+    echo $pidfile
+    PIDS[${#PIDS[@]}]=$pidfile
+    if [ ! -f $pidfile ]; then
+        echo "daemon did NOT run"
+        echo "see logs in $logfile"
+        exit_daemon
+    else
+        pid=$(cat $pidfile)
+        echo "daemon running at $pid"
+    fi
+}
+
 function exit_daemon {
     echo "shutdown received: stopping daemon"
-    $SERVICE-daemon.sh stop $MODULE
+    for((iter=0;iter<${#SERVICES[@]};iter++)) {
+        SERVICE=${SERVICES[$iter]}
+        MODULE=${MODULES[$iter]}
+        echo "shutting down $SERVICE $MODULE"
+        $SERVICE-daemon.sh stop $MODULE
+    }
     exit 0
 }
 
-while [ -e /proc/$pid ]; do
-    sleep 1;
-done
+trap exit_daemon SIGINT SIGTERM
 
+echo "========== ACTIVE =========="
+echo "all services running..."
+MODULO=${#PIDS[@]}
+ITER=0
+while [ -e ${PID[$ITER]} ]; do
+    ITER=$((ITER+1))
+    ITER=$((ITER%MODULO))
+    sleep 5;
+done
